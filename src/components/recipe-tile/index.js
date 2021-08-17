4ac048f4a3eb869c4c132250a54310df1bd20e78
Rohan Goel <rohangoel107@gmail.com>
2021-08-17 19:12:18 +0530

    project ready

---
+++ src/components/recipe-tile/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export default function RecipeTile({ recipe }) {
+  return (
+    recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+      <div
+        className="recipeTile"
+        onClick={() => window.open(recipe["recipe"]["url"])}
+      >
+        <img className="recipeTile__img" src={recipe["recipe"]["image"]} alt="" />
+        <p className="recipeTile__name" >
+          {recipe["recipe"]["label"]}
+        </p>
+      </div>
+    )
+  );
+}
